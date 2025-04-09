1d378be04f20c3ede06ffed5b8048468e5b9bf9b
MichaelConn27 <michael.connors@student.fairfield.edu>
2025-04-09 14:40:24 -0400

    first 4 graph methods

--- graph/DiGraphImpl.java
+++ graph/DiGraphImpl.java
@@ -8,10 +8,34 @@ public class DiGraphImpl implements DiGraph{
 
 	private List<GraphNode> nodeList = new ArrayList<>();
 
-	public Boolean addNode(GraphNode node) {return null;}
-	public Boolean removeNode(GraphNode node) {return null;}
-	public Boolean setNodeValue(GraphNode node, String newNodeValue) {return null;}
-	public String getNodeValue(GraphNode node) {return null;}
+	public Boolean addNode(GraphNode node) {
+		if (nodeList.contains(node)) return false;
+		nodeList.add(node);
+		return true;
+		}
+	public Boolean removeNode(GraphNode node) {
+		if (nodeList.contains(node)) {
+			List<GraphNode> neighborRemoval = nodeList.get(nodeList.indexOf(node)).getNeighbors();
+			neighborRemoval.forEach(g -> removeEdge(node, g));
+			nodeList.forEach(g -> {if(nodesAreAdjacent(g, node)) removeEdge(g, node);});
+			nodeList.remove(node);
+			return true;
+		}
+		return false;
+	}
+	public Boolean setNodeValue(GraphNode node, String newNodeValue) {
+		if (nodeList.contains(node)) {
+			nodeList.get(nodeList.indexOf(node)).setValue(newNodeValue);
+			return true;
+		}
+		return false;
+	}
+	public String getNodeValue(GraphNode node) {
+		if (nodeList.contains(node)) {
+			return nodeList.get(nodeList.indexOf(node)).getValue();
+		}
+		return null;
+	}
 	
 	public Boolean addEdge(GraphNode fromNode, GraphNode toNode, Integer weight) {return null;}
 	public Boolean removeEdge(GraphNode fromNode, GraphNode toNode) {return null;}
